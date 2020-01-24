7fbbf706fa8f6da3afa01c272ae8942d8e79c0e2
kate gleeson <kate.gleeson@thoughtbot.com>
2020-01-24 12:58:14 +0000

    install figaro

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 3.8'
   gem 'pry', '~> 0.12.2'
+  gem 'figaro', '~> 1.1'
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module CrypticCracker
-  class Application < Rails::Application  
+  class Application < Rails::Application
     config.load_defaults 6.0
   end
 end
