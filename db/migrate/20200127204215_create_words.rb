04881c50d861e0dad08d16da989c0d0f5904953b
kate gleeson <kate.gleeson@thoughtbot.com>
2020-01-31 17:00:59 +0000

    add seeds file

--- db/migrate/20200127204215_create_words.rb
+++ db/migrate/20200127204215_create_words.rb
@@ -2,7 +2,7 @@ class CreateWords < ActiveRecord::Migration[6.0]
   def change
     create_table :words do |t|
       t.string :written_form
-      t.integer :product
+      t.string :product
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+require 'prime'
+
+def logger
+  @logger ||= Logger.new('log/seeds.log')
+end
+
+def prime_map
+  ('a'..'z').to_a.zip(Prime.take(26)).to_h
+end
+
+def letters(word)
+  word.split('').select { |char| char.match(/[a-z]/) }
+end
+
+def primes(letters)
+  letters.map { |letter| prime_map[letter] }
+end
+
+def product(word)
+  letters = letters(word)
+  primes = primes(letters)
+  primes.inject(:*)
+end
+
+def rows
+  @rows ||= CSV.read('words.csv')
+end
+
+rows.each do |row|
+  begin
+    word = row[0]
+    product = product(word)
+    Word.create(written_form: word, product: product.to_s)
+  rescue => error
+    logger.info(word)
+    logger.info(error.message)
+  end
+end
