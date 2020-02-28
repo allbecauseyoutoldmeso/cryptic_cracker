a6c4aee8a67cf3f8007a16ac0a8cf65becb51a46
kate gleeson <kate.gleeson@thoughtbot.com>
2020-02-28 12:44:27 +0000

    add rack-cors gem

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,12 @@ Bundler.require(*Rails.groups)
 module CrypticCracker
   class Application < Rails::Application
     config.load_defaults 6.0
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3001'
+        resource '*', headers: :any, methods: [:get]
+      end
+    end
   end
 end
